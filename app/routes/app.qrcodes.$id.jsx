6f66f2e92c112724a7524d594e7ec7ab5e0db6d9
Kribas <carlousrimal@gmail.com>
2025-05-01 14:13:06 +0545

    created qrcode form component and added selectproduct and handlesave functions

--- app/routes/app.qrcodes.$id.jsx
+++ app/routes/app.qrcodes.$id.jsx
@@ -1,8 +1,9 @@
-import { useActionData, useLoaderData, useNavigation } from '@remix-run/react'
+import { useActionData, useLoaderData, useNavigate, useNavigation, useSubmit } from '@remix-run/react'
 import { getQRCode } from '../models/QRCode.server'
 import {authenticate} from '../shopify.server'
 import {data} from '@remix-run/node'
 import { useState } from 'react'
+import { Button, Page } from '@shopify/polaris'
 
 export async function loader({request, params}) {
     const {admin} = await authenticate.admin(request)
@@ -25,5 +26,54 @@ export default function QRCodeForm() {
     const [cleanFormState, setCleanFormState] = useState(qrCode)
     const isDirty = JSON.stringify(formState) !== JSON.stringify(cleanFormState)
     const nav = useNavigation()
+
+    const isSaving = nav.state === 'submitting' && nav.formData?.get('action') !== 'delete'
+    const isDeleting = nav.state === "submitting" && nav.formData?.get('action') == 'delete'
+
+    const navigate = useNavigate()
+
+    async function selectProduct() {
+        const products = await window.shopify.resourcePicker({
+            type: 'product',
+            action: 'select'
+        })
+
+        if(products) {
+            const {images,id,variants, title, handle} = products[0]
+
+            setFormState({
+                ...formState,
+                productId: id,
+                productVariantId: variants[0].id,
+                productTitle: title,
+                productHandle: handle,
+                productAlt: images[0]?.altText,
+                productImage: images[0]?.originalSrc
+            })
+         
+        }
+    }
+
+    const submit = useSubmit()
+
+    const handleSave = () => {
+        const data = {
+            title: formState.title,
+            productId: formState.productId || "",
+            productVariantId: formState.productVariantId || "",
+            productHandle: formState.productHandle || "",
+            destination: formState.destination
+        }
+        setCleanFormState({formState})
+        submit(data, {method: "POST"})
+    }
+
+    return (
+        <Page>
+            <ui-title-bar title={qrCode?.id ? "Edit QR code" : "Create New QR Code"}>
+                <button onClick={navigate("/app")} variant='breadcrumb'>QR codes</button>
+            </ui-title-bar>
+        </Page>
+    )
     
 }
